6297d38b618c171b154f12b51aba3854d51e2376
Brent Abrahams <brent_abrahams@yahoo.com>
2015-03-08 19:52:23 +0800

    Fixed CSS error

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'babrahams:editable-json',
-  version: '0.2.1',
+  version: '0.2.2',
   summary: 'Editable JSON for Meteor',
   // URL to the Git repository containing the source code for this package.
   git: 'https://github.com/JackAdams/meteor-editable-json.git',
