9767fb94e974cca2aea23eafab0d052d42f3472c
Brent Abrahams <brent_abrahams@yahoo.com>
2015-03-12 10:49:45 +0800

    Updated dependencies

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'babrahams:editable-json',
-  version: '0.3.5',
+  version: '0.3.6',
   summary: 'Editable JSON for Meteor',
   // URL to the Git repository containing the source code for this package.
   git: 'https://github.com/JackAdams/meteor-editable-json.git',
@@ -18,7 +18,7 @@ Package.onUse(function(api) {
   api.use('aldeed:template-extension@3.4.1','client');
   api.use('richsilv:pikaday@1.0.0','client');
   api.use('aldeed:collection2@2.3.2', {weak: true}); // This must go before: api.use('dburles:mongo-collection-instances@0.3.1');
-  api.use('dburles:mongo-collection-instances@0.3.1');
+  api.use('dburles:mongo-collection-instances@0.3.3');
   
   api.addFiles('editable-json-common.js');
   api.addFiles('editable-json.css','client');
